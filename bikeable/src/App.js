23b23378217474e6169436acf4c74f02c421ba72
ky1044 <ky1044@nyu.edu>
2020-04-10 15:19:43 +0900

    add loading indicator, footer

--- bikeable/src/App.js
+++ bikeable/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import Header from "./components/Header"
 import StationCard from "./components/StationCard"
+import Footer from "./components/Footer"
 
 class App extends React.Component{
   constructor(){
@@ -13,7 +14,8 @@ class App extends React.Component{
     latitude :40.7308,
     longitude:73.9973,
     lastUpdate :null,
-    timeSinceUpdate:null
+    timeSinceUpdate:null,
+    hasloaded:false
     }
     this.getStationStatus = this.getStationStatus.bind(this);
     this.setStationStatus = this.setStationStatus.bind(this);
@@ -30,14 +32,15 @@ class App extends React.Component{
     console.log("updated status")
     this.setState({ 
       stations:data.stationStatus,
-      lastUpdate: Date.now()
+      lastUpdate: Date.now(),
+      hasloaded:true
      });
   }
 
   componentDidMount() {
     this.getStationStatus()
     setInterval(() => this.setState({ timeSinceUpdate: Date.now()-this.state.lastUpdate}), 100)
-    setInterval(() => this.getStationStatus(), 40000)
+    setInterval(() => this.getStationStatus(), 30000)
       
   }
 
@@ -54,7 +57,7 @@ class App extends React.Component{
           lastUpdate = {this.state.timeSinceUpdate}
         />
         ) )}
-        {/* <p>{this.state.location}</p> */}
+        <Footer loaded = {this.state.hasloaded}/>
       </div>
     );
   }
---
+++ bikeable/src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+function Footer(props){
+    if (props.loaded){
+        return(
+
+            <div className = "foodster">
+                <div style={{margin:"auto",alignContent:"center"}}>
+        
+                <p style={{textAlign: "center",color:"#313131"}}>Made by Ken Yokokawa. Source code on <a href="https://github.com/ky1044/Bikeable" style={{color:"#313131"}}>Github</a>.</p>
+        
+                </div>
+            </div>
+        
+        
+            )
+    }
+    else{
+        return(
+            <h2 style={{textAlign: "center"}}><br/>Loading...</h2>
+        )
+    }
+	
+}
+export default Footer
