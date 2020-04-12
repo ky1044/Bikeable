27288424f50f3fd013a80efd4ec3a110e5519086
ky1044 <ky1044@nyu.edu>
2020-04-12 23:05:24 +0900

    fix bugs after merging speedystatus

--- api/getCurrentStatus.py
+++ api/getCurrentStatus.py
@@ -23,7 +23,9 @@ def get_nearby_stations(lat,lon,radius,max_length):
 				if nearby_stations[s]["distance"]>station_distance:
 					place=s
 					break
-			nearby_stations.insert(place,{"id": station.id,"name":station.name,"distance":station_distance,"mapsURL":""})
+			MapsURL = "https://www.google.com/maps/search/?api=1&query=Citi+Bike+"+station.name.replace(" ","+").replace("&","%26")
+			AppURL = "http://app.citibikenyc.com/S6Lr/IBV092JufD?station_id="+str(station.id)
+			nearby_stations.insert(place,{"id": station.id,"name":station.name,"distance":station_distance,"mapsURL":MapsURL})
 	return nearby_stations[:max_length]
 
 
