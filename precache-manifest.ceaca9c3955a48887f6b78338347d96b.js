23f8f99f04eff95f307d33afeb7465a387131998
ky1044 <ky1044@nyu.edu>
2020-05-21 20:16:20 +0900

    Updates

---
+++ precache-manifest.ceaca9c3955a48887f6b78338347d96b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5dbb21f804f33382f1d6d50132dc738",
+    "url": "/Bikeable/index.html"
+  },
+  {
+    "revision": "e004b64d396599ad9ea9",
+    "url": "/Bikeable/static/css/main.f3295c98.chunk.css"
+  },
+  {
+    "revision": "b01dfbc8728fad764cac",
+    "url": "/Bikeable/static/js/2.aabcb228.chunk.js"
+  },
+  {
+    "revision": "f3a464b094a42227540b3ef9a73a0401",
+    "url": "/Bikeable/static/js/2.aabcb228.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e004b64d396599ad9ea9",
+    "url": "/Bikeable/static/js/main.1b93ecd4.chunk.js"
+  },
+  {
+    "revision": "18e26230869049e59fb0",
+    "url": "/Bikeable/static/js/runtime-main.0e6a2a49.js"
+  }
+]);
